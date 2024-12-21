6416ed48c1aa548441d0e1eecea89bbd9f1de5c4
Arish <webdeveloper2@amvionlabs.com>
2024-12-21 14:41:55 +0530

    das

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 
 // config.js
-export const backendPort = 90;
+export const backendPort = 80;
 export const baseURL = `${window.location.protocol}//${window.location.hostname}:${backendPort}/MMU/`;
